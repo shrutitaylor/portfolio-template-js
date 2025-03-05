ee74de6b111d39e9a89680c9539c5537a29868d1
shrutitaylor <shruti128770@gmail.com>
2025-03-05 22:48:13 +1100

    gh pages installed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: "/portfolio-template-js/",
   css: {
     postcss: './postcss.config.js', // Ensure PostCSS is linked
   },
